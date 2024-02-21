1cd47a42b90bde7f9908de0bd4758cd36dc6e475
connect-Parvin <parvin.akter0190@gmail.com>
2024-02-21 18:52:34 +0600

    Nav bar adding

---
+++ src/Components/NavBar/Nav1.jsx
@@ -0,0 +1,18 @@
+
+
+const Nav1 = () => {
+    return (
+        <div className="bg-[#171e3c] sticky -top-1 z-50">
+            <div className="max-w-7xl mx-auto flex items-center justify-between h-10 ">
+                <span>
+                    <h1 className="text-sm font-bold text-white">Bagistro <span className="font-normal">Admin</span></h1>
+                </span>
+                <span>
+                    <h1 className="text-sm font-bold text-white border-b border-b-emerald-600">Admin  <span className="font-normal">Panal</span></h1>
+                </span> 
+            </div> 
+        </div>
+    );
+};
+
+export default Nav1;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import {
-  createBrowserRouter,
+ 
   RouterProvider,
 } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import Router from './Router/Router';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={Router} />
   </React.StrictMode>,
 )
